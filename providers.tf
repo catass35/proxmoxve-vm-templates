d4a4a8de93d59456565a8a7880944e15a5c3bef5
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-22 11:48:57 +0100

    Update providers.tf
    init

--- providers.tf
+++ providers.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.73.2"
+    }
+  }
+}
 
+variable "api_token" {
+   type=string
+}
+
+provider "proxmox" {
+  endpoint  = "https://192.168.1.58:8006/"
+  api_token = "terraform-prov@pve!terraform-api=${var.api_token}"
+  insecure  = true
+  ssh {
+    agent    = true
+    username = "terraform-prov"
+  }
+}
