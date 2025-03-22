ce38969333647b7c07de7a79b3cc7e9f180747ff
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-22 13:16:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+variable "proxmox_user" {
+  description = "The Proxmox API user"
+  type        = string
+}
 
+variable "api_token" {
+   type=string
+}
+
+variable "storage_pool" {
+  description = "The storage pool to use for the VM disk"
+  type        = string
+}
+
+##variable "ssh_public_key_file" {
+##  description = "Path to the SSH public key file"
+##  type        = string
+##}
